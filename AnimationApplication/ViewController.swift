6a2ca8e4d9f70868f559bb5f601f2541f9cf13ed
gleb <belkov.glebka@yandex.ru>
2024-04-28 19:20:58 +0300

    Initial Commit

---
+++ AnimationApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationApplication
+//
+//  Created by Глеб Бельков on 28.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
